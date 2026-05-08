e59edff2bee26316ac5b14deb6d595dcd4d53a9a
argoneuscze <argoneuscze@gmail.com>
2026-05-08 10:51:44 +0200

    hyprland cleanup

--- dot_config/hypr/executable_fix-xdph.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-sleep 4
-killall -e xdg-desktop-portal-hyprland
-killall xdg-desktop-portal
-/usr/libexec/xdg-desktop-portal-hyprland &
-sleep 4
-/usr/libexec/xdg-desktop-portal &
-
